fbfbddb0b72d08e5a3d6443d240aac16648d6902
Rithvika <rithvika451@gmail.com>
2025-10-07 13:31:49 +0530

    this is the final commit and app is ready

--- app.py
+++ app.py
@@ -17,5 +17,12 @@ def predict_api():
     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     return jsonify(output[0])
+@app.route("/predict",methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="the house price is {}".format(output))
 if __name__=="__main__":
     app.run(debug=True)
